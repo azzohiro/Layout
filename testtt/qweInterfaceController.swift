42f56712c2b8db04e5b62dc37635293e3bc5aa25
Azzo Hiro <azzohiro.dev@gmail.com>
2022-09-01 16:52:10 +0700

    wewewe

---
+++ testtt/qweInterfaceController.swift
@@ -0,0 +1,30 @@
+//
+//  qweInterfaceController.swift
+//  Layout
+//
+//  Created by Azzo Hiro on 01/09/22.
+//
+
+import WatchKit
+import Foundation
+
+
+class qweInterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
