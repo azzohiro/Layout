b0a10e5ef060b1570037f66519304a699aaf1767
Azzo Hiro <azzohiro.dev@gmail.com>
2022-09-01 16:55:02 +0700

    add apple watch

---
+++ eweweViewController.swift
@@ -0,0 +1,29 @@
+//
+//  eweweViewController.swift
+//  Layout
+//
+//  Created by Azzo Hiro on 01/09/22.
+//
+
+import UIKit
+
+class eweweViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
